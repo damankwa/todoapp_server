5f5cc6cfc7bc29b9236c1175495172ea320a4d65
david <damankwa@github.com>
2021-03-28 00:29:23 -0400

    commit added post and get examples

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const bodyParser= require('body-parser');
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+
+
+// Create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.use(express.static('public'));
+app.get('/index.htm', function (req, res) {
+   res.sendFile( __dirname + "/" + "index.htm" );
 })
+
+
+app.get('/process_get', function (req, res) {
+   // Prepare output in JSON format
+   response = {
+      first_name:req.query.first_name,
+      last_name:req.query.last_name
+   };
+   console.log(response);
+   res.end(JSON.stringify(response));
+})
+
+
+app.post('/process_post', urlencodedParser, function (req, res) {
+   // Prepare output in JSON format
+   response = {
+      first_name:req.body.first_name,
+      last_name:req.body.last_name
+   };
+   console.log(response);
+   res.end(JSON.stringify(response));
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
